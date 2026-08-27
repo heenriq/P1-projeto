d10925a03ab5f866b9c830330136fa68d7637159
PEDRO HENRIQUE DOS SANTOS <pedro_santos191@estudante.sesisenai.org.br>
2026-08-26 21:12:49 -0300

    eita site uow

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5500"
+
 app.use(cors());
 app.use(express.json());
 
